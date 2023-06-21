bf91c948c2b0fb0b8486575fcec3b2a13f562cc6
clakr <clarktolosa@gmail.com>
2023-06-21 09:56:52 +0800

    mobile, desktop done, to deploy

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,4 @@
-import "./style.css";
+import "./style.scss";
 
 function changeTextContent(element: HTMLTitleElement | Element | null) {
   if (!element) return;
@@ -12,4 +12,20 @@ function changeTextContent(element: HTMLTitleElement | Element | null) {
 
   const heading = document.querySelector(".sr-only");
   changeTextContent(heading);
+
+  const details = document.querySelectorAll("details");
+  details.forEach((detail) => {
+    detail.addEventListener("click", function (event) {
+      event.preventDefault();
+
+      const openedDetail = document.querySelector("details[open]");
+      openedDetail?.removeAttribute("open");
+
+      if (this !== openedDetail) {
+        this.setAttribute("open", "");
+      } else {
+        details[1].setAttribute("open", "");
+      }
+    });
+  });
 })();
